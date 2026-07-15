1a1539c66115ba0fba7d670daa2bd073acba13dd
Matthew_MCN <matthewmcnally181@gmail.com>
2026-07-14 23:34:59 -0400

    Create swadesh.js

---
+++ swadesh.js
@@ -0,0 +1,14 @@
+const SWADESH = [
+  "I","you","we","this","that","who","what","not","all","many",
+  "one","two","big","long","small","woman","man","fish","bird",
+  "dog","tree","seed","leaf","root","skin","blood","bone","fat",
+  "egg","horn","tail","feather","hair","head","ear","eye","nose",
+  "mouth","tooth","tongue","claw","foot","knee","hand","belly",
+  "neck","breast","heart","liver","drink","eat","bite","see",
+  "hear","know","sleep","die","kill","swim","fly","walk","come",
+  "lie","sit","stand","give","say","sun","moon","star","water",
+  "rain","stone","sand","earth","cloud","smoke","fire","ash",
+  "burn","road","mountain","red","green","yellow","white","black",
+  "night","day","year","warm","cold","full","new","good","round",
+  "dry","name"
+];
